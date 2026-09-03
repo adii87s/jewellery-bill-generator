c11236d359acd69d8a99e86d21f0a06e3d7a9fc2
adii87s <vegalsharma87@gmail.com>
2026-09-03 21:22:24 +0530

    Add FRONTEND_ORIGIN to CORS allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        FRONTEND_ORIGIN,
     ],
     allow_credentials=True,
     allow_methods=["*"],
